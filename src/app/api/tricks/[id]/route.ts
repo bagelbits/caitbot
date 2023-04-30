528d95e733d243f6e2ac42ac36ff9424051502bd
Chris Ward <cdurianward@gmail.com>
2023-04-29 19:15:40 -0700

    Move everything to a src folder

--- src/app/api/tricks/[id]/route.ts
+++ src/app/api/tricks/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "../../../../src/lib/prisma";
+import prisma from "../../../../lib/prisma";
 
 export async function GET(request: Request) {
   const id = request.url.slice(request.url.lastIndexOf("/") + 1);
